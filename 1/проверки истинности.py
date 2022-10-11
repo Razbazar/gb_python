00265298ea588f753f5508c6597c0c8f19f80ce4
odin <odinzzz@gmail.com>
2022-10-11 15:45:22 +0300

    фикс проверки инстинности

--- 1/проверки истинности.py
+++ 1/проверки истинности.py
@@ -1,10 +1,9 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-io = (0, 1)
 lst = ['ложно', 'истинно']
-for x in io:
-    for y in io:
-        for z in io:
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
             a = not (x or y or z)
             b = (not x) and (not y) and (not z)
             t = a == b
