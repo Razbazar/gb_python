2b11abc0a316eb414ca4fa003e51a4808060ee88
odin <odinzzz@gmail.com>
2022-10-18 12:33:51 +0300

    ДОбавил Сумму нечетных позиций

---
+++ Homework/3/сумма нечетных позиций.py
@@ -0,0 +1,10 @@
+from random import sample, randint
+
+lst = sample(range(30), randint(4, 12))
+print('Сгенерирован список чисел', *lst)
+suma = 0
+odd_lst = []
+for i in range(1, len(lst), 2):
+    suma += lst[i]
+    odd_lst.append(str(lst[i]))
+print(f'Сумма элементов на нечетных позициях {" + ".join(odd_lst)} = {suma}')
