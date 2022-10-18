9418df8c038394e1abca20fb24f31a62db594cc7
odin <odinzzz@gmail.com>
2022-10-18 12:34:28 +0300

    Какие то доработки по второй задачи чисел полседовательности

--- Homework/2/числа последовательности.py
+++ Homework/2/числа последовательности.py
@@ -1,8 +1,10 @@
 n = int(input("Введите число N "))
 summa = 0
 lst = []
+
+
 def formula(x):
-    return round(pow((1 + 1 / x), x), 2)
+    return round(pow((1+1/x), x), 2)
 
 
 for i in range(1, n + 1):
@@ -11,3 +13,6 @@ for i in range(1, n + 1):
     lst.append(el)
 
 print(f"Список последовательности: {lst}", f"сумма его элементов: {summa:.2f}", sep="\n")
+
+x = 494
+"tire"
